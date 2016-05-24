862bd6bc9ed61896ab2b97414f9b9316a3a042b9
eeshanl <eeshan@uw.edu>
2016-05-24 02:51:56 +0000

    yay

--- mag.py
+++ mag.py
@@ -6,6 +6,105 @@ import math
 import json
 import requests
 
+
+
+
+
+def initMags():
+    global initMagnitude0, initMagnitude1
+    
+    xMag0init = 0
+    xMag1init = 0
+
+    yMag0init = 0
+    yMag1init = 0
+
+    zMag0init = 0
+    zMag1init = 0
+
+
+    realX0 = 0
+    realX1 = 0
+
+    realY0 = 0
+    realY1 = 0
+
+    realZ0 = 0
+    realZ1 = 0
+    time.sleep(1)
+
+    for i in range(100):
+
+        data0 = bus0.read_i2c_block_data(0x0E, 0x01, 6)
+        data1 = bus1.read_i2c_block_data(0x0E, 0x01, 6)
+
+        # x
+        xMag0init = data0[0] * 256 + data0[1]
+        if xMag0init > 32767:
+            xMag0init -= 65536
+            
+        realX0 += xMag0init
+        
+        xMag1init = data1[0] * 256 + data1[1]
+        if xMag1init > 32767:
+            xMag1init -= 65536
+            
+        realX1 += xMag1init
+        
+        # y
+        yMag0init = data0[2] * 256 + data0[3]
+        if yMag0init > 32767:
+            yMag0init -= 65536
+            
+        realY0 += yMag0init
+        
+        yMag1init = data1[2] * 256 + data1[3]
+        if yMag1init > 32767:
+            yMag1init -= 65536
+
+        realY1 += yMag1init
+
+        # z
+        zMag0init = data0[4] * 256 + data0[5]
+        if zMag0init > 32767:
+            zMag0init -= 65536
+
+        realZ0 += zMag0init
+        
+        zMag1init = data1[4] * 256 + data1[5]
+        if zMag1init > 32767:
+            zMag1init -= 65536
+
+        realZ1 += zMag1init
+
+
+
+    
+    realX0 = realX0 / 100.0
+    realY0 = realY0 / 100.0
+    realZ0 = realZ0 / 100.0
+
+    realX1 = realX1 / 100.0
+    realY1 = realY1 / 100.0
+    realZ1 = realZ1 / 100.0
+
+    initMagnitude0 = math.sqrt(math.pow(realX0, 2) + math.pow(realY0, 2) + math.pow(realZ0, 2))
+    initMagnitude1 = math.sqrt(math.pow(realX1, 2) + math.pow(realY1, 2) + math.pow(realZ1, 2))
+
+    json_data["initMag0"] = initMagnitude0
+    json_data["initMag1"] = initMagnitude1
+
+    with open("config.json", 'w') as outfile:
+        json.dump(json_data, outfile)
+
+    while requests.post("http://" + server_url + ":8080/garages", data=json_data).status_code != 200:
+       pass
+
+    initialTime = time.time()
+
+
+
+
 with open("config.json") as json_file:
     json_data = json.load(json_file)
 
@@ -23,18 +122,19 @@ time.sleep(0.5)
 log = open("log.txt", "w+")
 
 initMagnitude0 = json_data["initMag0"]
-initMagnitude1 = json_data["initMag0"]
+initMagnitude1 = json_data["initMag1"]
 
 carInBoolean = True
 carOutBoolean = True
 
 netCars = 0
 
-
-carData = {"garageID": "Team Swag Garage", "netCars": 0}
+carData = {"garageID": json_data["garageID"], "spots": 0, "address": json_data["address"]}
 
 if (initMagnitude0 == 0 and initMagnitude1 == 0):
+    print "init called"
     initMags()
+
 
 while True:
     try:
@@ -69,32 +169,39 @@ while True:
         magnitude0 = math.sqrt(math.pow(xMag0, 2) + math.pow(yMag0, 2) + math.pow(zMag0, 2))
         magnitude1 = math.sqrt(math.pow(xMag1, 2) + math.pow(yMag1, 2) + math.pow(zMag1, 2))
 
-        if ((magnitude0 - initMagnitude0) < -30 and carInBoolean):
+
+        print "dif0: " + str(magnitude0 - initMagnitude0)
+        print "dif1: " + str(magnitude1 - initMagnitude1)
+        
+
+        if ((magnitude0 - initMagnitude0) < -10 and carInBoolean): # should be 30
+            print "0 Updated"
             carInBoolean = False
             netCars -= 1
-        if ((magnitude1 - initMagnitude1) < -30 and carOutBoolean):
+        if ((magnitude1 - initMagnitude1) < -10 and carOutBoolean): # should be 30
+            print "1 Updated"
             carOutBoolean = False
             netCars += 1
 
 
-        if (Math.abs(magnitude0 - initMagnitude0) <= 10):
+        if (math.fabs(magnitude0 - initMagnitude0) <= 10): # should be 10
             carInBoolean = True
 
-<<<<<<< HEAD
-        if (Math.abs(magnitude1 - initMagnitude1) <= 10):
-=======
-        if (Math.abs(magnitude1 - initMagnitude1) <= 10)
->>>>>>> 4def34fbb4bfddba584ad4c5073373545a37683d
+
+        if (math.fabs(magnitude1 - initMagnitude1) <= 10): # should be 10
             carOutBoolean = True
 
 
         currTime = time.time()
-        if (currTime - initialTime > 59):
+        if (currTime - initialTime > 3):
             initialTime = currTime
-            carData["netCars"] = netCars
+            carData["spots"] = netCars
             netCars = 0
-            r = requests.post(server_url, data=json.dumps(carData))
-            log.write("Current Time: " + currTime + ", " + "netCars: " + carData["netCars"] + ", " + "Status Code: " + r.status_code + ", " + "Text: " + r.text)
+
+            print "requesting"
+            r = requests.put("http://" + server_url + ":8080/garages/" + carData["garageID"], data=carData)
+            print r.text
+            #log.write("Current Time: " + currTime + ", " + "netCars: " + carData["netCars"] + ", " + "Status Code: " + r.status_code + ", " + "Text: " + r.text)
             # TODO: reset log file every month
             # TODO: new thread for communicating with the server
 
@@ -105,66 +212,3 @@ while True:
         print("woops2222222222222222222222222222222222222222222222222222222222222")
     time.sleep(0.0166667)
 
-
-def initMags():
-
-    xMag0init = 0
-    xMag1init = 0
-
-    yMag0init = 0
-    yMag1init = 0
-
-    zMag0init = 0
-    zMag1init = 0
-
-    for i in range(100):
-
-        data0 = bus0.read_i2c_block_data(0x0E, 0x01, 6)
-        data1 = bus1.read_i2c_block_data(0x0E, 0x01, 6)
-
-        # x
-        xMag0init += data0[0] * 256 + data0[1]
-        if xMag0init > 32767:
-            xMag0init -= 65536
-        xMag1init += data1[0] * 256 + data1[1]
-        if xMag1init > 32767:
-            xMag1init -= 65536
-
-        # y
-        yMag0init += data0[2] * 256 + data0[3]
-        if yMag0init > 32767:
-            yMag0init -= 65536
-        yMag1init += data1[2] * 256 + data1[3]
-        if yMag1init > 32767:
-            yMag1init -= 65536
-
-        # z
-        zMag0init += data0[4] * 256 + data0[5]
-        if zMag0init > 32767:
-            zMag0init -= 65536
-        zMag1init += data1[4] * 256 + data1[5]
-        if zMag1init > 32767:
-            zMag1init -= 65536
-
-    xMag0init = xMag0init / 100
-    yMag0init = yMag0init / 100
-    zMag0init = zMag0init / 100
-
-    xMag1init = xMag1init / 100
-    yMag1init = yMag1init / 100
-    zMag1init = zMag1init / 100
-
-    initMagnitude0 = math.sqrt(math.pow(xMag0init, 2) + math.pow(yMag0init, 2) + math.pow(zMag0init, 2))
-    initMagnitude1 = math.sqrt(math.pow(xMag1init, 2) + math.pow(yMag1init, 2) + math.pow(zMag1init, 2))
-
-    json_data["initMag0"] = initMagnitude0
-    json_data["initMag1"] = initMagnitude1
-
-    with open("config.json", 'w') as outfile:
-        json.dump(json_data, outfile)
-
-
-    while (requests.post(server_url + "/init", data=json.dumps(json_data)).status_code != 200):
-        pass
-
-    initialTime = time.time()
